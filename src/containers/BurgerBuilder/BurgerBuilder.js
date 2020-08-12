6c20547a2bb3e3627a1975ea02bccefe3f66a727
Luis Riveros <luis66riv@gmail.com>
2020-08-12 09:54:59 -0400

    fix(Aux): rename file to Auxiliary-windows error

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import Aux from '../../hoc/Aux'
+import Aux from '../../hoc/Auxiliary'
 import Burger from '../../components/Burger/Burger';
 import BuilderControls from '../../components/BuilderControl/BuilderControls';
 
